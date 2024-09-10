ea4a6b49f1cbe46858e83e700503a52edaf48876
PedroMaxis <pedro11sales14@gmial.com>
2024-09-10 20:59:13 -0300

    feat: change default

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,6 @@ const PORT = 5000;
 
 // Middleware
 app.use(cors());
-app.use(cors({ origin: 'https://gallery-viewer-5i4v.vercel.app' }));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
